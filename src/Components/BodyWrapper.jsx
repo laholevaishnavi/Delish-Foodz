d8a503098c5345a9107ff47a871bdd0ae864c8e9
laholevaishnavi <laholevaishnavi11@gmail.com>
2025-04-29 16:57:44 +0530

    CORS issue resolved!

--- src/Components/BodyWrapper.jsx
+++ src/Components/BodyWrapper.jsx
@@ -14,9 +14,11 @@ const BodyWrapper = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
-      );
+      const response = await fetch('https://delish-foodz-backend.onrender.com/api/restaurants?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
+
+      //  fetch(
+      //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      // );
       const data = await response.json();
 
       const restaurantsFromCard1 =
