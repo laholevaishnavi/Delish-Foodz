c1cca991d730b9989ea27aa5fe361a4d3c2a6bcc
laholevaishnavi <laholevaishnavi11@gmail.com>
2025-05-01 14:01:36 +0530

    clear cart functionality added

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Itemlist from "./Itemlist";
-
-// how to read cart items? useSelector!!
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  console.log(cartItems);
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   return (
     <>
-      <h1 className="text-center font-semibold m-4 text-2xl">Cart</h1>
-      <div className="w-6/12  m-auto text-center">
-        <Itemlist className="" items={cartItems} />
+      <h1 className="text-center font-semibold m-6 text-3xl text-gray-800">Cart</h1>
+      <div className="w-full sm:w-10/12 md:w-8/12 lg:w-6/12 m-auto p-6 bg-white rounded-lg shadow-lg">
+      {cartItems.length == 0 ? <h1>Your Cart is Empty</h1>:  <button
+          onClick={handleClearCart}
+          className="bg-rose-600 text-gray-100 font-semibold px-4 py-2 rounded-md shadow-md hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-rose-500 transition duration-300 mb-6"
+        >
+          Clear Cart
+        </button>}
+      
+        <Itemlist items={cartItems} />
       </div>
     </>
   );
--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -1,34 +1,26 @@
-import React from 'react'
-const Shimmer = () => {
+import React from 'react';
+
+const ShimmerCard = () => {
   return (
-    <>
-    <div className='Shimmer-envelop'>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-    <div className='Shimmer-card'></div>
-
-    
+    <div className="shimmer-card rounded-lg p-4 m-4 w-60 bg-white shadow-md">
+      <div className="shimmer-image rounded-md mb-4 bg-gray-300 h-36 w-full animate-shimmer"></div>
+      <div className="shimmer-line rounded-md mb-2 bg-gray-300 h-6 w-3/4 animate-shimmer"></div>
+      <div className="shimmer-line rounded-md mb-2 bg-gray-300 h-4 w-full animate-shimmer"></div>
+      <div className="shimmer-line rounded-md mb-2 bg-gray-300 h-4 w-full animate-shimmer"></div>
+      <div className="shimmer-line rounded-md bg-gray-300 h-4 w-5/6 animate-shimmer"></div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Shimmer
+const Shimmer = () => {
+  const shimmerCards = Array(10).fill(0);
+  return (
+    <div className="shimmer-envelop flex flex-wrap justify-center items-center">
+      {shimmerCards.map((_, index) => (
+        <ShimmerCard key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default Shimmer;
